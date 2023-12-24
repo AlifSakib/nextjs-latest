dd3772ecfb5776b1eeac684b5f87db428f71e55d
alifsakib <alifsakib@gmail.com>
2023-12-24 15:54:04 +0600

    initial commit

---
+++ src/utils/check-node-version.ts
@@ -0,0 +1,16 @@
+const { engines } = require("./../../package.json");
+const semver = require("semver");
+const execSync = require("child_process").execSync;
+const currentNodeVersion = process.versions.node;
+if (!semver.satisfies(currentNodeVersion, engines.node)) {
+  console.log(
+    `You are using Node ${currentNodeVersion}, but this version of 
+        "@Next.js Project"
+      requires Node ${engines.node}.`
+  );
+  process.exit(1);
+} else {
+  console.log(
+    `You are using Node ${currentNodeVersion}, which is compatible with ${"@Next.js Project"}`
+  );
+}
